65bdfa3c24954972d19b764588ff4152e0d053cf
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 02:22:54 +0800

    adding bootstrap_flash to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def bootstrap_flash
+   flash_messages = []
+   flash.each do |type, message|
+     type = :success if type == :notice
+     type = :error   if type == :alert
+     text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message.html_safe, :class => "alert fade in alert-#{type}")
+     flash_messages << text if message
+   end
+   flash_messages.join("\n").html_safe
+ end
+ 
   def pdf_image_tag(image, options = {})
     options[:src] = File.expand_path(Rails.root) + '/public' + image
     tag(:img, options)
