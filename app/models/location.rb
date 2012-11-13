37151110ba63ff74257d58ffc311166eb938ab7d
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 04:31:58 +0800

    location accepting nested attributes for units

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :unit_attributes
   has_many :units
+  accepts_nested_attributes_for :units
 end
 
