e11236d1b2a89654da795a6addc6ccaa49107b17
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 04:31:36 +0800

    unused setting for activeadmin_association

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,6 @@ module Etracker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.aa_associations.autocomplete = false
   end
 end
