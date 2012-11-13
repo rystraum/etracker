81c5cb96008d1c4300b76a5bc88ef577a5c85689
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 02:30:34 +0800

    custom form for creating users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,9 +1,23 @@
 ActiveAdmin.register User, as: "Steward" do
   filter :email
-  
+
   index do
     column :id
     column :email
     default_actions
+  end
+
+  form do |f|
+    f.inputs "Auth details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+
+    f.inputs "Personal details" do
+      f.input :name
+      f.input :contact_number
+    end
+    f.buttons
   end
 end
