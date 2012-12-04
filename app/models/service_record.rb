ea47d877bcf213ed3a181c18bce80ef3d691076e
Rystraum Gamonez <rystraum@gmail.com>
2012-12-04 14:42:32 +0800

    model service_record

--- app/models/service_record.rb
+++ app/models/service_record.rb
@@ -1,4 +1,4 @@
 class ServiceRecord < ActiveRecord::Base
-  attr_accessible :comment, :date_checked, :name, :unit_id
+  attr_accessible :comment, :date_checked, :name, :unit_id, :present_day_value
   belongs_to :unit
 end
