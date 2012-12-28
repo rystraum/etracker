e9836f09e2b45aeb88ea3c7e99f849ff057dce75
Rystraum Gamonez <rystraum@gmail.com>
2012-12-28 22:37:16 +0800

    unit_attributes -> units_attributes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :description, :name, :unit_attributes
+  attr_accessible :description, :name, :units_attributes
   has_many :units
   accepts_nested_attributes_for :units
 end
-
